9b670ad6485819448690f56f72e50dd47ed86177
Michael Andrzejewski <michael@jetskitechnologies.com>
2021-01-01 13:46:12 -0500

    Docblock and code cleanup. No functional changes.

--- plg_user_oneloginsaml/oneloginsaml.php
+++ plg_user_oneloginsaml/oneloginsaml.php
@@ -1,43 +1,39 @@
 <?php
-
 /**
- * @package     OneLogin SAML.Plugin
- * @subpackage  User.oneloginsaml
- *
+ * @package     Joomla-Saml
+ * @subpackage  plg_iser_oneloginsaml
+ * 
  * @copyright   Copyright (C) 2019 OneLogin, Inc. All rights reserved.
  * @license     MIT
+ * @author      Michael Andrzejewski<michael@jetskitechnologies.com>
  */
 use Joomla\CMS\String\PunycodeHelper;
 
-    class PlgUserOneloginsaml extends Joomla\CMS\Plugin\CMSPlugin {
-        /*
-          public function onUserAuthenticate($credentials, $options, &$response)
-          {
-          // We redirect to initiate the SSO Login
-          // $app = JFactory::getApplication();
-          // $sso_url = JURI::base().'plugins/user/oneloginsaml/oneloginsaml.php?sso';
-          // $app->redirect($sso_url);
-          }
-         */
-        /**
-         * 
-         * @param \Joomla\CMS\User\User $user
-         * @param string $key the attribute name to be updated
-         * @param string $value the attribute value to be updated
-         */
-        public function onUserUpdateInfo($user, $key, $value ) {
-            switch($key) {
-                case 'Username':
-                    $user->username = $value;
-                    break;
-                case 'Email' :
-                    $user->email = PunycodeHelper::emailToPunycode($value);
-                    break;
-                case 'Name' :
-                    $user->name = $value;
-                    break;
-            }
-            $user->save();
+/**
+ * class to handle attribute setting via the samlLogin Lib
+ */
+class PlgUserOneloginsaml extends Joomla\CMS\Plugin\CMSPlugin {
+
+    /**
+     * very simple user attribute updating/matching from the samlLoginLib
+     *
+     * @param \Joomla\CMS\User\User $user User to set values on
+     * @param string $key the attribute name to be updated
+     * @param string $value the attribute value to be updated
+     */
+    public function onUserUpdateInfo($user, $key, $value) {
+        switch ($key) {
+            case 'Username':
+                $user->username = $value;
+                break;
+            case 'Email' :
+                $user->email = PunycodeHelper::emailToPunycode($value);
+                break;
+            case 'Name' :
+                $user->name = $value;
+                break;
         }
+        $user->save();
+    }
 
 }
